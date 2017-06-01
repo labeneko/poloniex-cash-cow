069e1c1136e8a28e5d400ab96c8c7fdb79665d4f
津野　貴大 <takahiro_tsuno@dwango.co.jp>
2017-06-01 11:48:24 +0900

    display holdings

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
+import env from 'node-env-file';
 import express from 'express';
+import ApiClient from 'poloniex-api-node';
 
+env('./.env');
+
+const apiClient = new ApiClient(process.env.API_KEY, process.env.API_SECRET);
 const router = express.Router();
 
 /* GET index page. */
 router.get('/', (req, res, next) => {
-  res.render('index', {
-    title: 'Express'
+  apiClient.returnCompleteBalances('all', function (err, balances) {
+    console.log(balances);
+    const filteredBalances = Object.keys(balances)
+    .filter(key => balances[key]['btcValue'] > 0)
+    .reduce((prev, key) => {
+      prev[key] = balances[key];
+      return prev;
+    }, {});
+    const totalBtcValue = Object.keys(filteredBalances)
+    .reduce((prev, key) => prev + parseFloat(balances[key]['btcValue']), 0)
+    res.render('index', {
+      balances: filteredBalances,
+      totalBtcValue
+    });
   });
 });
 
